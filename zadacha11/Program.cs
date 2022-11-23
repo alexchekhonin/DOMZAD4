68f9399d87da4e29a1b52e224be6b6f6878f1237
Александр Чехонин <2007cheh@mail.ru>
2022-11-23 23:10:04 +0300

    zad11

---
+++ zadacha11/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 11. 
+// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+// Выводим слуайное число из отрезка 10-99
+// Определяем 1 и 2 цифру числа
+// Сравниваем 1 и 2 цифру числа
+// Выводим наибольшую
+
+Console.Clear();
+int value = new Random().Next(10, 100);
+int a=value / 10;
+int b = value % 10;
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+Console.WriteLine(value);
