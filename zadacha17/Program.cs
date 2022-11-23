725921061ed60caf300d614e6fb30d3351de300d
Александр Чехонин <2007cheh@mail.ru>
2022-11-23 23:34:06 +0300

    zad17

---
+++ zadacha17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 17. По двум заданным числам проверять
+//            является ли одно квадратом другого.
+
+Console.WriteLine("Enter value1: ");
+int value1 =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter value2: ");
+int value2 =Convert.ToInt32(Console.ReadLine());
+
+if(value1*value1 == value2)
+{
+    Console.WriteLine("value2 является квадратом value1");
+}
+else
+
+if(value2*value2 == value1)
+{
+    Console.WriteLine("value1 является квадратом value2");
+}
+else Console.WriteLine("Ни одно из чисел не является квадратом другого");
