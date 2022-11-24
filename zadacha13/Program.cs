50950337a342025384640f5375264778625e8623
Александр Чехонин <2007cheh@mail.ru>
2022-11-24 09:01:22 +0300

    z13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int a=value / 100; //первое
+int b=value / 10 % 10; //второе
+int c=value % 10; // третье
+
+Console.WriteLine(value);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
