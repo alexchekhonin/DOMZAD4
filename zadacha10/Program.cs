876462dba2ccb4176a0ea45f8be3ab9221e1a0c4
Александр Чехонин <2007cheh@mail.ru>
2022-11-24 08:17:04 +0300

    zad10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Enter value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int a=value / 100; //первое
+int b=value / 10 % 10; //второе
+int c=value % 10; // третье
+
+Console.Write(value);
+Console.Write("->");
+Console.Write(b);
