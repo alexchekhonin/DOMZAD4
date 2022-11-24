e2bf87164592ddf160ada16ecd109822ff6276c0
Александр Чехонин <2007cheh@mail.ru>
2022-11-24 07:37:56 +0300

    zad15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задаа 15. Дано число. Проверить кратно ли оно 7 и 23
+// Вводим число
+// Делим на 7 и 23
+// Если делится - то кратно
+// Если нет - не кратно
+
+Console.WriteLine("Enter value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int a = value % 7;
+int b = value % 23;
+
+if (a == 0 & b == 0)
+{
+    Console.WriteLine("Кратно");
+}
+
+else
+{
+    Console.WriteLine("Не кратно");
+}
